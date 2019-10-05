b2259a17578793b646865bafe4bdd03961adf008
Aramis9999 <54870634+Aramis9999@users.noreply.github.com>
2019-10-05 14:18:09 -0500

    Added:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hook.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Hook.java
@@ -3,15 +3,32 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class Hook {
     Servo hook;
-    public Hook(Servo hook){
+    
+   
+    
+    public Hook(Servo hook) {
+        
         this.hook = hook;
     }
 
-    public Servo getHook(){
+    public Servo getHook() {
+        
         return hook;
     }
 
-    public void setHook(Servo hook){
+    public void setHook(Servo hook) {
+         
         this.hook = hook;
     }
+    
+   public void setPosition(position) {
+       
+       if (position < 0) {
+           position = 0;
+       }
+       else if (position > 1) {
+           
+           position = 1;
+       }         
+               
 }
